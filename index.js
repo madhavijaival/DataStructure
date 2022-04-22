e74b7de4b2700c0232d8f42913ff1cecc7afe220
Mirhaa <madhavi.jaival113@gmail.com>
2022-04-22 04:54:35 +0000

    Format the code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const nemo =['nemo'];
-function find(array){
-  let t0= performance.now();
-  for (let i =0; i<array.length; i++){
-    if (array[i] === 'nemo'){
+const nemo = ['nemo'];
+function find(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
       console.log("Found NEMO!")
     }
-  let t1= performance.now();
-    console.log("Calling nemo took " + (t1-t0)+" ms time")
-}
+    let t1 = performance.now();
+    console.log("Calling nemo took " + (t1 - t0) + " ms time")
+  }
 }
 find(nemo);
